fb42b1786d5d23d82a66b3cc0e3c7df95d0238d8
Nathaniel Cauldwell <ncauldwell@gmail.com>
2017-11-07 12:37:40 -0500

    Kinvey backend, new pages.
    Update to use Kinvey backend.
    Add items, users, and winning pages.

--- README.md
+++ README.md
@@ -1,12 +1,33 @@
 BidHub Web Administration
 ===================
-Small little web app to see the current status, item winners, and some neat stats for a BidHub auction. BidHub is HubSpot's open-source silent auction app - for an overview of the project, [check out our blog post about it](http://dev.hubspot.com/blog/building-an-auction-app-in-a-weekend)!
+Small web app to see the current status, item winners, and some neat stats for a BidHub auction. BidHub is HubSpot's open-source silent auction app - for an overview of the project, [check out their blog post about it](http://dev.hubspot.com/blog/building-an-auction-app-in-a-weekend)!
 
-This will be entirely useless to you if you haven't set up Parse and followed the instructions in the [BidHub Cloud Code repository](https://github.com/HubSpot/BidHub-CloudCode).
+This will be entirely useless to you if you haven't set up Kinvey and followed the instructions in the [BidHub Cloud Code repository](https://github.com/ncauldwell/BidHub-CloudCode).
 
 ![Screenshot](http://i.imgur.com/0hPpRLBl.png)
 
 ## Setup
-Assuming you've already set up Parse, get your application ID and REST API key (not your client key) from Parse > Settings > Keys. Then, just `git clone` this repository and edit *controllers.js*, replacing `<your app id>` and `<your REST API key>` with the appropriate values. 
+Assuming you've already set up Kinvey, you'll need to get your application key from the Kinvey console and you'll also need to create a new Role and a new User.
 
-Open index.html in Chrome and you're good to go! Put this on any web server for easy access.
+## Admin Role and User
+### Admin Role
+1. In the Kinvey Console for your app, go to Identity > Roles and [[+ Add a Role]].
+2. Give it a name and description and click Save.
+3. Now go to Identity > Users and click the Settings gear icon.
+4. Under Permissions, click [[+Add role access]] and choose the new role you just created. Be sure to grant that role **Always** permissions for each CRUD (Create, Read, Update, Delete) action set. Save.
+
+### Admin User
+1. In the Kinvey Console for your app, go to Identity > Users and [[+ Add User]].
+2. Enter the username and password for your administrator user and [[Save]].
+3. Click the checkbox on the new user's row and in the Roles section choose the admin role you created earlier and click [[Assign Role]].
+
+## Finish Up
+`git clone` this repository and edit *controllers.js*, *index.html*, and *items.html* replacing `<your kinvey app key>`, `<your auction admin username>`, and `<your auction admin password>` with the appropriate values.
+
+Open index.html in Chrome and you're good to go! Put this on any web server for easy access, but not really. These files have the plain text username and password of a user with complete control of the users collection. This should not just be "put on any web server". 
+
+## Page Listing
+*`index.html` shows basic auction and item stats; it also has a form for adding new items to the auction, though I'm not certain that form works - I moved that function to a separate page.
+*`items.html` shows all the auction items and allows you to add new items.
+*`users.html` lists all registered users and allows you to assign them a bidder number (or remove their bidder number).
+*`winning.html` lists all items in the auction with the current high bid & bidder.
--- controllers.js
+++ controllers.js
@@ -1,20 +1,28 @@
 /**
  * Created by hubspot on 11/16/14.
+ * Modified by ncauldwell
  */
 var auctionApp = angular.module('auctionApp', []);
 
 auctionApp.controller('ItemCardsCtrl', function ($scope) {
 
-    $.ajax({
-        url: "https://api.parse.com/1/classes/Item",
-        type: "GET",
-        beforeSend: function(xhr){
-            xhr.setRequestHeader('X-Parse-Application-Id', '<your app id>');
-            xhr.setRequestHeader('X-Parse-REST-API-Key', '<your REST API key, NOT YOUR CLIENT KEY>');
-        },
+    // TODO: The admin user has full CRUD permissions on the users collection.
+    //       The username/password should NOT be listed here in plain text.
+    //       Find some other, more secure way of doing this.
+    var kinveyAppKey = "<your kinvey app key>";
+    var adminUsername = "<your auction admin username>";
+    var adminPassword = "<your auction admin password>";
+    var auth = window.btoa(adminUsername+":"+adminPassword);
+
+	$.ajax({
+		type: "GET",
+		url: "https://baas.kinvey.com/appdata/"+kinveyAppKey+"/items/?query={}&sort=name",
+		beforeSend: function(xhr){
+			xhr.setRequestHeader('Authorization', 'Basic '+auth);
+		},
         success: function(result) {
             $scope.$apply(function(){
-                $scope.items = result.results;
+                $scope.items = result;
                 var totalRaised = 0;
                 var totalStartPrice = 0;
                 var numBids = 0;
@@ -73,9 +81,54 @@ auctionApp.controller('ItemCardsCtrl', function ($scope) {
                 $scope.itemCount = $scope.items.length;
                 $scope.noBidCount = noBidCount;
             });
-
         }
     });
+
+	$.ajax({
+		type: "GET",
+		url: 'https://baas.kinvey.com/user/'+kinveyAppKey+'/?query={"email":{"$nin":["'+adminUsername+'"]}}&sort={"bidderNumber":1,"sort_first":1,"sort_last":1}',
+		beforeSend: function(xhr){
+			xhr.setRequestHeader('Authorization', 'Basic '+auth);
+		},
+        success: function(result) {
+            $scope.$apply(function(){
+                $.each(result, function(i, val) {
+	               	result[i].id = result[i]._id;
+                    result[i].ect = result[i]._kmd.ect;
+//                    result[i].bidderNumber = ("000" + result[i].bidderNumber).slice(-3);
+                });
+                $scope.users = result;
+            });
+        }
+    });
+
+	$scope.updateUser = function(id, bidnum)
+    {
+        var updatedUser = JSON.stringify({
+        	"bidderNumber": bidnum,
+        	"setBidNum": 1
+		});
+
+        var kinveyAppKey = "";
+        var adminUsername = "";
+        var adminPassword = "";
+        var auth = window.btoa(adminUsername+":"+adminPassword);
+    
+        $.ajax({
+            type: "PUT",
+			url: "https://baas.kinvey.com/user/"+kinveyAppKey+"/" + id + "/",
+			beforeSend: function(xhr){
+				xhr.setRequestHeader('Authorization', 'Basic '+auth);
+			},
+			data: updatedUser,
+			success: function () {
+				location.reload();
+			},
+			contentType:"application/json; charset=utf-8",
+			dataType:"json"
+		});
+	}        
+
 
     $scope.buildCSV = function() {
         var headers = ["item name", "donor name", "winner1", "bid1", "winner2", "bid2", "winner3", "bid3", "winner4", "bid4",
@@ -109,8 +162,6 @@ auctionApp.controller('ItemCardsCtrl', function ($scope) {
 
             data.push([name, donor, winnerName, highestBids]);
         });
-
-
 
         var csvContent = "data:text/csv;charset=utf-8,";
         csvContent += headers.join(",") + "\n";
@@ -156,4 +207,6 @@ auctionApp.filter('noFractionCurrency',
                 }
                 return value.substring(0, sep) + ')';
             };
-        } ]);
+        }
+    ]
+);
